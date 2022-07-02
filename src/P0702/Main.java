ad922d3dc45f658c0c190bd592742292fc10c890
sgpublic <3300633844@qq.com>
2022-07-02 08:33:06 +0800

    0702

---
+++ src/P0702/Main.java
@@ -0,0 +1,23 @@
+package P0702;
+
+import java.util.HashSet;
+import java.util.Scanner;
+
+/**
+ * @author sgpublic
+ * @Date 2022/7/2 8:26
+ */
+public class Main {
+    public static void main(String[] args) {
+        HashSet<Integer> map = new HashSet<>();
+        Scanner input = new Scanner(System.in);
+        int l = input.nextInt() + 1, m = input.nextInt();
+        while (m-- > 0) {
+            int u = input.nextInt(), v = input.nextInt();
+            for (int i = u; i <= v; i++) {
+                map.add(i);
+            }
+        }
+        System.out.println(l - map.size());
+    }
+}
