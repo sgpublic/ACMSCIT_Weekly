be91b81781160a1d6ab08601c41bd0b004be11c4
sgpublic <3300633844@qq.com>
2022-07-05 09:36:27 +0800

    0705

---
+++ src/P0705/Main.java
@@ -0,0 +1,25 @@
+package P0705;
+
+import java.util.Scanner;
+
+/**
+ * @author sgpublic
+ * @Date 2022/7/5 9:24
+ */
+public class Main {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int index = 0, max = 8;
+        for (int i = 1; i <= 7; i++) {
+            int tmp = input.nextInt() + input.nextInt();
+            if (tmp > max) {
+                index = i; max = tmp;
+            }
+        }
+        if (max <= 8) {
+            System.out.println(0);
+        } else {
+            System.out.println(index);
+        }
+    }
+}
